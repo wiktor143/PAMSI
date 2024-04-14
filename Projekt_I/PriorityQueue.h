3fb9e0d8a8b2d856e54a29bd8f2543c96acb61c4
Wiktor Kwiatkowski <wiktorkwiatkowski143@gmail.com>
2024-04-14 23:17:59 +0200

    PriorityQueue.h is done

--- Projekt_I/PriorityQueue.h
+++ Projekt_I/PriorityQueue.h
@@ -4,17 +4,20 @@
 
 // Struktura jednego node'a
 struct Node {
+    // Zmienna do przechowywania pakietu
     std::string packet;
+    // Priorytet
     unsigned int priority;
+    // Wskaźnik na poprzedzający node
     Node *next;
 };
 
 class PriorityQueue {
    private:
+    // Wskaźnik na początek listy jednokierunkowej
     Node *head;
 
-    // Funkcja służąca do usunięcia node'a o największym(w naszym przypadku
-    // pierwszego) priorytecie.
+    // Funkcja służąca do usunięcia node'a o największym(w naszym przypadku pierwszego) priorytecie.
     void pop();
 
    public:
@@ -24,11 +27,12 @@ class PriorityQueue {
     // Funckja służąca do dodania kolejnego node'a do kolejki priorytetowej
     // oraz ustawienia go zgodnie z priorytetem.
     void push(unsigned int priority, const std::string &packet);
+
     // Funkcja służąca do wyświetlenia stanu kolejki priorytetowej.
     void display();
 
     // Funkcja służąca do sprawdzenie czy kolejka priorytetowa jest pusta.
-    bool isEmpty();
+    bool isEmpty() const;
 
     // Funckja służąca do usunięcia całej kolejki priorytetowej.
     void clear();
