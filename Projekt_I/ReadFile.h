2422a316b7212bb6e5f8e1fe67df5f2b8846691b
Wiktor Kwiatkowski <wiktorkwiatkowski143@gmail.com>
2024-04-14 23:31:33 +0200

    ReadFile.h is done

--- Projekt_I/ReadFile.h
+++ Projekt_I/ReadFile.h
@@ -5,19 +5,19 @@
 #include <iostream>
 // Struktura jednego pakietu
 struct Packet {
+    // Zmienna do przechowywania pakietu
     std::string packet;
+    // Priorytet
     unsigned int priority;
 };
 
 // Funkcja służąca do odczytania pliku podanego przez użytkownika
-// oraz podzielenia tekstu na ilość obliczonych pakietów
-// przez MaxPacketsCount i "zapakowania" odpowiedniej ilości bajtów
-// do pakietu.
+// oraz podzielenia tekstu na "N" obliczonych pakietów
+// przez MaxPacketsCount oraz "zapakowania" odpowiedniej ilości bajtów do pakietu.
 // W sytuacji błędu zwraca wartość 1.
-
 int readPackets(const std::string &file_name, int offset, int message_size, int packet_size, Packet *packets, int max_packets);
-// Funkcja obliczająca ilość maksymalnych pakietów na podstawie
-// message_size oraz packet_size.
+
+// Funkcja obliczająca ilość maksymalnych pakietów na podstawie message_size oraz packet_size.
 // Zwraca ilość pakietów
 int MaxPacketsCount(int message_size, int packet_size);
 
