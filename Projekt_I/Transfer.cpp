e3bf4f253067e9d15db402c46b7b7ddc8af96242
Wiktor Kwiatkowski <wiktorkwiatkowski143@gmail.com>
2024-04-14 23:56:12 +0200

    Transfer.cpp is done

--- Projekt_I/Transfer.cpp
+++ Projekt_I/Transfer.cpp
@@ -1,17 +1,41 @@
 #include "Transfer.h"
 
-Transfer::Transfer(){};
+Transfer::Transfer() {}
 
-void Transfer::sendPacket(Packet *packets, int length) {
-    for (int i = 0; i < length; i++) {
-        priorityQueue.push(packets[i].priority, packets[i].packet);
+void Transfer::sendPackets(Packet *packets, const int length) { shufflePackets(packets, length); }
+
+void Transfer::receivePackets(Packet *packets, int max_packets) {
+    for (int i = 0; i < max_packets; i++) {                         // Tworzymy zadaną ilość pakietów
+        priorityQueue.push(packets[i].priority, packets[i].packet); // Dodajemy node do naszej kolejki priorytetowej
     }
 }
 
-void Transfer::displayReceived(){
-    priorityQueue.display();
+void Transfer::displayReceived() {
+    priorityQueue.display(); // Wyświetlamy cała zawartość kolejki priorytetową
+    std::cout << std::endl;  // Po wyświetleniu wiadomości zrób spację
 }
 
-void Transfer::freeMemeory(){
-    priorityQueue.clear();
+void Transfer::shufflePackets(Packet *packets, const int length) {
+    srand(time(NULL));
+
+    for (int i = 0; i < length; ++i) {     // Przejdź przez wyszystkie pakiety 
+        int swap_index = rand() % length;  // Losujemy indeks do podmiany
+        Packet temp = packets[i];          // Zmienna pomocnicza podczas zamiany
+        packets[i] = packets[swap_index];  // Podmieniamy zawartości pod danymi indeksami
+        packets[swap_index] = temp;        // Koniec zamiany 
+    }
 }
+
+void Transfer::displayShuffledPackets(Packet *packets, const int length) {
+    std::cout << "=============================SENT==============================" << std::endl;
+    for (int i = 0; i < length; ++i) { // Wyświetlamy zawartość każdego pakietu
+        std::cout << packets[i].packet;
+    }
+    std::cout << std::endl
+              << "===========================RECEIVED============================"
+              << std::endl;
+}
+
+void Transfer::freeMemeory() { 
+    priorityQueue.clear(); // Zwalniamy pamięc zaalokowaną dla kolejki priorytetowej
+ }
