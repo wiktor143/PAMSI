ab1221533a8cfb04cfe2b84c748a8c0a4728503c
Wiktor Kwiatkowski <wiktorkwiatkowski143@gmail.com>
2024-04-14 23:44:36 +0200

    Script for testing

---
+++ Projekt_I/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Początkowy rozmiar wiadomości
+start_message_size=5000
+# Końcowy rozmiar wiadomości
+end_message_size=9980000
+# Krok zwiększania rozmiaru wiadomości
+step=25000
+
+# Usunięcie poprzednich wyników z plików
+>results.txt
+>times.txt
+
+# Pętla testująca program dla różnych rozmiarów wiadomości
+for ((message_size = $start_message_size; message_size <= $end_message_size; message_size += $step)); do
+    echo "Test dla rozmiaru wiadomości: $message_size B"
+    ./driver tekst.txt 0 $message_size 100 >>results.txt
+    # Pobranie ostatniej linii z pliku wynikowego i wyświetlenie
+    last_line=$(tail -n 1 results.txt)
+    echo "$last_line" >>times.txt
+done
