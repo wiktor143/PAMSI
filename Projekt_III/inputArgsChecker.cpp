606e0e9e5602901f0fa17da2084ae0d2250dba68
Wiktor Kwiatkowski <wiktorkwiatkowski143@gmail.com>
2024-06-11 02:51:04 +0200

    works

--- Projekt_III/inputArgsChecker.cpp
+++ Projekt_III/inputArgsChecker.cpp
@@ -1,21 +1,25 @@
 #include "inputArgsChecker.h"
 
+// Konstruktor klasy inputArgsChecker
 inputArgsChecker::inputArgsChecker(int argc, char* argv[])
     : valid(false), randomSeed(0), ipAddress("localhost"), ipPort(12345) {
+    // Przypisanie nazwy programu
     progName = argv[0];
-    std::cout << "argc: " << argc << std::endl;
+    // Sprawdzenie liczby argumentów, jeśli jest niepoprawna, zwraca false
     if (argc < 4 || argc > 7) {
         return;
     }
-
+    if (argc == 6) {
+        return;
+    }
+    // Przypisanie wartości argumentów do zmiennych 
     try {
         interfaceType = argv[1];
         turn = argv[2];
         depth = std::atoi(argv[3]);
-        if (argc == 5) {
+        if (argc >= 5) {
             randomSeed = std::atoi(argv[4]);
         }
-        std::cout << "czytanie arg: " << randomSeed << std::endl;
         if (argc == 7) {
             ipAddress = argv[5];
             ipPort = std::atoi(argv[6]);
@@ -25,28 +29,35 @@ inputArgsChecker::inputArgsChecker(int argc, char* argv[])
                   << std::endl;  // Wystąpił błąd
         return;
     }
+    // Sprawdzenie poprawności argumentów
     if ((interfaceType == "GUI" || interfaceType == "NET") &&
         (turn == "WHITE" || turn == "BLACK") && depth > 0) {
         valid = true;
     }
 }
 
+// Metoda sprawdzająca, czy argumenty wejściowe są poprawne.
 bool inputArgsChecker::isValid() const { return valid; }
 
+// Metoda zwracająca typ interfejsu określony przez argumenty wejściowe.
 std::string inputArgsChecker::getInterfaceType() const { return interfaceType; }
 
+// Metoda zwracająca, który gracz zaczyna grę określony przez argumenty wejściowe.
 std::string inputArgsChecker::getTurn() const { return turn; }
 
+// Metoda zwracająca głębokość analizy algorytmu określoną przez argumenty wejściowe.
 int inputArgsChecker::getDepth() const { return depth; }
 
+// Metoda zwracająca ziarno losowe określone przez argumenty wejściowe.
 long int inputArgsChecker::getRandomSeed() const { return randomSeed; }
 
+// Metoda zwracająca adres IP określony przez argumenty wejściowe.
 std::string inputArgsChecker::getIpAddress() const { return ipAddress; }
 
+// Metoda zwracająca numer portu określony przez argumenty wejściowe.
 int inputArgsChecker::getIpPort() const { return ipPort; }
 
+// Metoda wyświetlająca informacje o tym, jak używać programu.
 void inputArgsChecker::howToUse() const {
-    std::cerr << "Poprawne wywołanie programu:" << std::endl
-              << "  " << progName << " interface turn depth [random_seed] [ip_address ip_port]"
-              << std::endl;
+    std::cerr << "Poprawne wywołanie programu:" << std::endl << "  " << progName << " interface turn depth [random_seed] [ip_address ip_port]" << std::endl;
 }
--- Projekt_III/inputArgsChecker.h
+++ Projekt_III/inputArgsChecker.h
@@ -3,25 +3,52 @@
 #include <iostream>
 
 class inputArgsChecker {
-   public:
-    inputArgsChecker(int argc, char* argv[]);
-    bool isValid() const;
-    std::string getInterfaceType() const;
-    std::string getTurn() const;
-    int getDepth() const;
-    long int getRandomSeed() const;
-    std::string getIpAddress() const;
-    int getIpPort() const;
-    void howToUse() const;
-
    private:
-    std::string progName;
-    bool valid;
-    std::string interfaceType;
-    std::string turn;
-    int depth;
-    long int randomSeed;
-    std::string ipAddress;
-    int ipPort;
+    std::string progName;       // Nazwa programu.
+    bool valid;                 // Zmienna przechowująca informację o poprawności argumentów.
+    std::string interfaceType;  // Typ interfejsu.
+    std::string turn;           // Informacja o tym, który gracz zaczyna.
+    int depth;                  // Głębokość analizy algorytmu.
+    long int randomSeed;        // Ziarno losowe.
+    std::string ipAddress;      // Adres IP.
+    int ipPort;                 // Numer portu.
+
+   public:
+    // Konstruktor klasy przyjmujący liczbę argumentów oraz tablicę argumentów.
+    // Parametry:
+    // - argc: Liczba argumentów.
+    // - argv: Tablica argumentów.
+    inputArgsChecker(int argc, char* argv[]);
+
+    // Metoda sprawdzająca, czy argumenty wejściowe są poprawne.
+    // Zwraca true, jeśli argumenty są poprawne, w przeciwnym razie false.
+    bool isValid() const;
+
+    // Metoda zwracająca typ interfejsu określony przez argumenty wejściowe.
+    // Zwraca typ interfejsu jako string.
+    std::string getInterfaceType() const;
+
+    // Metoda zwracająca, który gracz zaczyna grę określony przez argumenty wejściowe.
+    // Zwraca "black" jeśli zaczyna czarny, w przeciwnym razie "white".
+    std::string getTurn() const;
+
+    // Metoda zwracająca głębokość analizy algorytmu określoną przez argumenty wejściowe.
+    // Zwraca głębokość analizy jako int.
+    int getDepth() const;
+
+    // Metoda zwracająca ziarno losowe określone przez argumenty wejściowe.
+    // Zwraca ziarno losowe jako long int.
+    long int getRandomSeed() const;
+
+    // Metoda zwracająca adres IP określony przez argumenty wejściowe.
+    // Zwraca adres IP jako string.
+    std::string getIpAddress() const;
+
+    // Metoda zwracająca numer portu określony przez argumenty wejściowe.
+    // Zwraca numer portu jako int.
+    int getIpPort() const;
+
+    // Metoda wyświetlająca informacje o tym, jak używać programu.
+    void howToUse() const;
 };
 #endif
